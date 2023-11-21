adb4d4f250f985858a1711061d585be5ca3a66b6
Santiago Arango Galeano <santiagoarangogaleano.1204@gmail.com>
2023-11-21 00:30:09 -0500

    Se crea la ventana 3 y el botón para navegar a este desde la ventana2.py #16

---
+++ ventana3.py
@@ -0,0 +1,10 @@
+import sys
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QHBoxLayout, QFormLayout, QLineEdit, QPushButton, QDialog, QDialogButtonBox, QVBoxLayout
+
+from PyQt5.QtGui import QColor, QFont, QPixmap, QIcon
+from PyQt5.QtCore import Qt
+
+class Ventana3(QMainWindow):
+    pass
